7b902a9fdab57db351c7004f580840ed8e8bb6d3
Yuvraj Singh <124923698+yuvrajsingh8048@users.noreply.github.com>
2023-12-23 08:49:29 +0530

    first commit

---
+++ 01_Basic/01_File Path/02_filePath.js
@@ -0,0 +1,9 @@
+/*
+Nodejs Path Module
+ allows you to interact with file path easily.
+ property and methods used to manipulate path in the file system.
+*/
+
+const path = require('path')
+console.log(path.sep)
+console.log(path.delimiter)
